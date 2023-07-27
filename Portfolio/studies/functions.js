bc851a33af1ad2b5ad409c7bba0e4d0627d9b452
coco1234562 <91998627+coco1234562@users.noreply.github.com>
2023-07-27 17:04:14 -0500

    Update functions.js

--- Portfolio/studies/functions.js
+++ Portfolio/studies/functions.js
@@ -34,6 +34,44 @@ let product = multiply(3, 4); // Result: 12
    - Arguments: Arguments are the actual values passed to the function when it is called. These values are assigned to the corresponding parameters in the function's declaration.
 */
 
+
+//function declaration
+function greet(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+
+//b) Function Invocation or Execution:
+
+greet("John");
+
+
+
+//Difference between Parameters and Arguments:
+
+
+//para
+/*
+In this example, a and b are parameters defined in the function declaration for add. 
+They act as placeholders for the values that the function expects to receive when called. 
+Within the function body, a and b are treated as local variables.
+*/
+function add(a, b) {
+  return a + b;
+}
+
+
+
+//Arguments
+/*
+In this example, 3 and 5 are arguments passed to the add function when it is called. 
+These values are assigned to the parameters a and b inside the function. 
+So, a becomes 3 and b becomes 5, and the function returns the sum of the two arguments, which is 8.
+*/
+const result = add(3, 5);
+console.log(result); // Output: 8
+
+
 //3. The syntax for a NAMED function:
    function functionName(parameter1, parameter2) {
        // Function code goes here
@@ -62,4 +100,45 @@ Scope:
    Closures occur when a function "remembers" its lexical scope even when the function is executed outside that scope. This means that if a function returns another function or if an inner function is referenced and held in memory somewhere, it retains access to the variables and data in its outer function's scope. The outer function's scope becomes a part of the closure, allowing the inner function to access and use those variables even after the outer function has finished executing. This behavior enables data to persist within closures even if the original function has completed its execution.
   
    */
+
+
+//closure example
+function outerFunction() {
+  let outerVar = "I'm from the outer function";
+
+  function innerFunction() {
+    let innerVar = "I'm from the inner function";
+    console.log(outerVar); // Accessing the variable from the outer function (closure)
+    console.log(innerVar); // Accessing the variable from the inner function
+  }
+
+  return innerFunction;
+}
+
+const closureFunc = outerFunction();
+closureFunc(); // This will log the values of outerVar and innerVar from the inner function
+
+
+//function returning function example
+function greetGenerator(greeting) {
+  function greet(name) {
+    return `${greeting}, ${name}!`;
+  }
+
+  return greet;
+}
+
+const sayHello = greetGenerator("Hello");
+const sayHi = greetGenerator("Hi");
+
+console.log(sayHello("Alice")); // Output: "Hello, Alice!"
+console.log(sayHi("Bob")); // Output: "Hi, Bob!"
+
+
+
+
+
+
+
+
 
